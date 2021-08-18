f03e798b4f8abfeb8fdd2d3238900b4a09140187
AliakbarSu <aliakbar.su@hotmail.com>
2021-08-18 18:14:04 +1200

    Add getStarted guide component

--- src/components/containers/home/index.vue
+++ src/components/containers/home/index.vue
@@ -2,17 +2,20 @@
   <div>
     <Carousel />
     <Section />
+    <GetStarted />
   </div>
 </template>
 
 <script>
 import Carousel from './components/UI/carousel/carousel.vue'
 import Section from './components/UI/section/section.vue'
+import GetStarted from './components/UI/getStarted/getStarted.vue'
 
 export default {
   components: {
     Carousel,
-    Section
+    Section,
+    GetStarted
   }
 }
 </script>
