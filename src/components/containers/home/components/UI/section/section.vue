959bf673611636a1050f20c4bef307c1ffc0124e
AliakbarSu <aliakbar.su@hotmail.com>
2021-08-18 18:06:26 +1200

    Add section component

---
+++ src/components/containers/home/components/UI/section/section.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-window class="elevation-1 section" vertical>
+      <v-window-item>
+        <p>
+          MPT enables you to practice for your AMC, MQCM test by taking mock
+          test which have been created by GP professionns who have worked in
+          this field for over 20 years.
+        </p>
+      </v-window-item>
+    </v-window>
+
+    <v-window class="elevation-2 section" vertical>
+      <v-window-item>
+        <p>
+          MPT enables you to practice for your AMC, MQCM test by taking mock
+          test which have been created by GP professionns who have worked in
+          this field for over 20 years.
+        </p>
+      </v-window-item>
+    </v-window>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.section {
+  width: 95%;
+  margin: 5% auto;
+  padding: 15px;
+}
+</style>
--- src/components/containers/home/index.vue
+++ src/components/containers/home/index.vue
@@ -1,9 +1,18 @@
 <template>
-    <div>This is MPT</div>
+  <div>
+    <Carousel />
+    <Section />
+  </div>
 </template>
 
 <script>
+import Carousel from './components/UI/carousel/carousel.vue'
+import Section from './components/UI/section/section.vue'
+
 export default {
-    
+  components: {
+    Carousel,
+    Section
+  }
 }
 </script>
