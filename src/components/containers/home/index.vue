0ad2881abf6d2e41bbcfe9f01165b9df03f1fdd5
AliakbarSu <aliakbar.su@hotmail.com>
2021-08-25 10:12:37 +1200

    Add hero to home component

--- src/components/containers/home/index.vue
+++ src/components/containers/home/index.vue
@@ -1,21 +1,23 @@
 <template>
   <div>
-    <Carousel />
+    <!-- <Carousel /> -->
+    <Hero />
     <Section />
     <GetStarted />
   </div>
 </template>
 
 <script>
-import Carousel from './components/UI/carousel/carousel.vue'
+// import Carousel from './components/UI/carousel/carousel.vue'
 import Section from './components/UI/section/section.vue'
 import GetStarted from './components/UI/getStarted/getStarted.vue'
+import Hero from './components/UI/hero/hero.vue'
 
 export default {
   components: {
-    Carousel,
     Section,
-    GetStarted
+    GetStarted,
+    Hero
   }
 }
 </script>
