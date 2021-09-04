ee85a9a0afd2ea625c6d391e47596b3434343817
AliakbarSu <aliakbar.su@hotmail.com>
2021-09-04 20:46:37 +1200

    Update example component

--- src/components/containers/home/components/examples/examples.vue
+++ src/components/containers/home/components/examples/examples.vue
@@ -1,23 +1,6 @@
 <template>
-  <div class="d-flex justify-space-between align-center pa-2 flex-wrap">
-    <v-card :elevation="2" width="100%" class="secondary mt-2"
-      ><v-img
-        width="100%"
-        src="https://res.cloudinary.com/dxuf2ssx6/image/upload/q_auto/v1630726770/MLE/MLE-test-question.jpg"
-      ></v-img>
-    </v-card>
-    <v-card :elevation="2" width="48%" class="secondary mt-2">
-      <v-img
-        width="100%"
-        src="https://res.cloudinary.com/dxuf2ssx6/image/upload/v1630727893/MLE/MLE-results-graphs-1.jpg"
-      ></v-img>
-    </v-card>
-    <v-card :elevation="2" width="48%" class="secondary mt-2">
-      <v-img
-        width="100%"
-        src="https://res.cloudinary.com/dxuf2ssx6/image/upload/v1630726619/MLE/MLE-results-graph-2.jpg"
-      ></v-img>
-    </v-card>
+  <div class="mt-16">
+    <v-img width="100%" src="@/assets/example-one.jpg"></v-img>
   </div>
 </template>
 
