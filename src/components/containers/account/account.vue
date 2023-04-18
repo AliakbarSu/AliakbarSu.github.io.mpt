325022a1e0bd373397790323654c93e43d06300e
Ali Sultani <aliakbar.su@hotmail.com>
2023-04-18 14:13:33 +1200

    Update account component

--- src/components/containers/account/account.vue
+++ src/components/containers/account/account.vue
@@ -1,35 +1,54 @@
 <template>
-  <v-container fluid class="my-2 fill-height">
-    <v-container class="elevation-2 fill-height align-start" fluid>
-      <v-container>
-        <v-row>
-          <v-col cols="2">
-            <v-card max-height="125" max-width="125" elevation="2">
-              <v-img :src="user.avatar" contain class="grey darken-4"></v-img>
-            </v-card>
-          </v-col>
-          <v-col cols="2" class="align-end d-flex">
-            <p>{{ user.name }}</p>
-          </v-col>
-          <v-col cols="12">
-            <v-container class="elevation-2 py-7 rounded-lg d-flex flex-wrap">
-              <p class="text-body2 mb-0 mr-2">Email:</p>
-              <p class="text-body1 mb-0 text-uppercase">{{ user.email }}</p>
-            </v-container>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-container>
-  </v-container>
+
+
+  <div class="mx-auto max-w-7xl pt-16 lg:flex lg:gap-x-16 lg:px-8">
+    <aside class="flex overflow-x-auto border-b border-gray-900/5 py-4 lg:block lg:w-64 lg:flex-none lg:border-0 lg:py-20">
+      <nav class="flex-none px-4 sm:px-6 lg:px-0">
+        <ul role="list" class="flex gap-x-3 gap-y-1 whitespace-nowrap lg:flex-col">
+          <li v-for="item in secondaryNavigation" :key="item.name">
+            <a @click="setItem(item.name)" :href="item.href" :class="[item.current ? 'bg-gray-50 text-indigo-600' : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50', 'group flex gap-x-3 rounded-md py-2 pl-2 pr-3 text-sm leading-6 font-semibold']">
+              <component :is="item.icon" :class="[item.current ? 'text-indigo-600' : 'text-gray-400 group-hover:text-indigo-600', 'h-6 w-6 shrink-0']" aria-hidden="true" />
+              {{ item.name }}
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+
+   <General v-if="currentItem == 'General'"/>
+   <Security v-if="currentItem == 'Security'"/>
+   <Plans v-if="currentItem == 'Plan'"/>
+   <Billing v-if="currentItem == 'Billing'"/>
+  </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { Dialog, DialogPanel, Switch, SwitchGroup, SwitchLabel } from '@headlessui/vue'
+import { Bars3Icon } from '@heroicons/vue/20/solid'
+import General from './components/General.vue'
+import Security from "./components/Security.vue"
+import Plans from "./components/Plans.vue"
+import Billing from "./components/Billing.vue"
+import {
+  BellIcon,
+  CreditCardIcon,
+  CubeIcon,
+  FingerPrintIcon,
+  UserCircleIcon,
+  UsersIcon,
+  XMarkIcon,
+} from '@heroicons/vue/24/outline'
 
-export default {
-  computed: {
-    user() {
-      return this.$store.getters.getUser
-    }
-  }
+const secondaryNavigation = [
+  { name: 'General', href: '#', icon: UserCircleIcon, current: true },
+  { name: 'Security', href: '#', icon: FingerPrintIcon, current: false },
+  { name: 'Plan', href: '#', icon: CubeIcon, current: false },
+  { name: 'Billing', href: '#', icon: CreditCardIcon, current: false },
+]
+
+const currentItem = ref("General")
+const setItem = (item: string) => {
+  currentItem.value = item
 }
 </script>
