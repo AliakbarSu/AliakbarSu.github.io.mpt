78520b7cb1d425c8a43d382622503890c62bdfa2
Ali Sultani <aliakbar.su@hotmail.com>
2023-04-21 13:56:46 +1200

    Remove test link from navbar

--- src/components/UI/navbar/navbar.vue
+++ src/components/UI/navbar/navbar.vue
@@ -132,11 +132,6 @@ export default {
               @click="goTo('/dashboard')"
               >Dashboard</a
             >
-            <a
-              href="#"
-              class="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-              >Tests</a
-            >
           </div>
         </div>
         <div class="flex items-center">
