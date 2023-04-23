07926191dcfd63df8db4de478a9fb6ab3af154da
Ali Sultani <aliakbar.su@hotmail.com>
2023-04-24 08:44:30 +1200

    Move controls to the right en

--- src/components/containers/test/components/UI/question-controls/questionControls.vue
+++ src/components/containers/test/components/UI/question-controls/questionControls.vue
@@ -1,28 +1,30 @@
 <template>
-  <span class="mt-5 isolate inline-flex rounded-md shadow-sm">
-    <button
-      @click="next"
-      type="button"
-      class="relative inline-flex items-center rounded-l-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-    >
-      Next
-    </button>
-    <button
-      v-if="canSkip"
-      @click="skip"
-      type="button"
-      class="relative -ml-px inline-flex items-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-    >
-      Skip
-    </button>
-    <button
-      @click="end"
-      type="button"
-      class="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-    >
-      End
-    </button>
-  </span>
+  <div>
+    <span class="mt-5 isolate inline-flex rounded-md shadow-sm">
+      <button
+        @click="next"
+        type="button"
+        class="relative inline-flex items-center rounded-l-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+      >
+        Next
+      </button>
+      <button
+        v-if="canSkip"
+        @click="skip"
+        type="button"
+        class="relative -ml-px inline-flex items-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+      >
+        Skip
+      </button>
+      <button
+        @click="end"
+        type="button"
+        class="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+      >
+        End
+      </button>
+    </span>
+  </div>
 </template>
 
 <script lang="ts">
